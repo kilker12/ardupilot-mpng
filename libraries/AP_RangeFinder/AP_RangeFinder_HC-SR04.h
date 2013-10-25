51b957e7bfa6d4dca05d6ac4b479280de9bd89e7
kilker12 <joey@mjhosting.net>
2013-10-24 22:17:42 -0400

    Create HC-SR04.h

---
+++ libraries/AP_RangeFinder/AP_RangeFinder_HC-SR04.h
@@ -0,0 +1,35 @@
+// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+#ifndef __AP_RANGEFINDER_HC-SR04_H__
+#define __AP_RANGEFINDER_HC-SR04_H__
+
+#include "RangeFinder.h"
+
+#define AP_RANGE_FINDER_HC-SR04_DEFAULT_ADDR   0x70
+
+#define AP_RANGEFINDER_HC-SR04               5
+#define AP_RANGE_FINDER_HC-SR04_MIN_DISTANCE  5
+#define AP_RANGE_FINDER_HC-SR04_MAX_DISTANCE  200
+
+class AP_RangeFinder_HC-SR04 : public RangeFinder
+{
+
+public:
+
+    // constructor
+    AP_RangeFinder_HC-SR04(FilterInt16 *filter);
+
+    // take_reading - ask sensor to make a range reading
+    bool            take_reading();
+
+    // read value from sensor and return distance in cm
+    int             read();
+
+    // heath
+    bool            healthy;
+
+protected:
+    uint8_t _addr;
+
+};
+#endif  // __AP_RANGEFINDER_HC-SR04_H__
