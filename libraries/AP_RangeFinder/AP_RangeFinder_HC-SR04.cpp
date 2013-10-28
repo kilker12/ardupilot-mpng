767792cbc2043c5a20b88e86a1a9475a4b402a70
kilker12 <joey@mjhosting.net>
2013-10-28 17:30:40 -0400

    Update AP_RangeFinder_HC-SR04.cpp

--- libraries/AP_RangeFinder/AP_RangeFinder_HC-SR04.cpp
+++ libraries/AP_RangeFinder/AP_RangeFinder_HC-SR04.cpp
@@ -34,7 +34,7 @@ extern const AP_HAL::HAL& hal;
 
 // Constructor //////////////////////////////////////////////////////////////
 
-AP_RangeFinder_MaxsonarI2CXL::AP_RangeFinder_HC-SR04( FilterInt16 *filter ) :
+AP_RangeFinder_HC-SR04::AP_RangeFinder_HC-SR04( FilterInt16 *filter ) :
     RangeFinder(NULL, filter),
     healthy(true),
 {
@@ -43,24 +43,43 @@ AP_RangeFinder_MaxsonarI2CXL::AP_RangeFinder_HC-SR04( FilterInt16 *filter ) :
 }
 
 // Public Methods //////////////////////////////////////////////////////////////
+// init - setup sonar stuffs
+void AP_RangeFinder_HC-SR04::init(void)
+{
+        // Sonar INIT
+        //=======================
+        PORTH&=B10111111; // H6 -d9  - sonar TX
+        DDRH |=B01000000;
+
+        PORTB&=B11101111; // B4 -d10 - sonar Echo
+        DDRB &=B11101111;
+        
+        // Timer5 initialized in APM_RC library
+/*
+        // div64 = 0.5 us/bit
+        // Using timer5, warning! Timer5 also share with RC PPM decoder
+        TCCR5A = 0; //standard mode with overflow at A and OC B and C interrupts
+        TCCR5B |= (1<<CS11); //Prescaler set to 8, resolution of 0.5us
+        TIMSK5 |= B00000111; // ints: overflow, capture, compareA
+        OCR5A = 65510; // approx 10m limit, 33ms period
+        OCR5B = 3000;*/
+}
 
 // read - return last value measured by sensor
-int AP_RangeFinder_HC-SR04::read()
+float AP_RangeFinder_HC-SR04::read(void)
 {
-    long duration, inches, cm;
-
-    hal.gpio->pinMode(trigPin, OUTPUT);
-    hal.gpio->write(trigPin, LOW);
-    delayMicroseconds(2);
-    hal.gpip->write(trigPin, HIGH);
-    delayMicroseconds(10);
-    hal.gpio->write(trigPin, LOW);
-
-    hal.gpio->pinMode(echoPin, INPUT);
-    duration = pulseIn(echoPin, HIGH);
- 
-    // convert the time into a distance
-    cm = microsecondsToCentimeters(duration);
-    
-    return cm
+   float result;
+   if (((sonar_data < 590) || (sonar_data > 59000)) && (pre_sonar_data > 0) ) {        //value must be 5cm > X < 5m
+      if (sonar_error_cnt > 50) {
+         result = 65490; // set as maximum value 5.55m - fallback to Baro (in arducopter.pde)
+      } else {
+         sonar_error_cnt++;
+         result = pre_sonar_data; // use previous valid data
+      }
+   } else {
+      sonar_error_cnt = 0; // Valid data received, reset counter
+      pre_sonar_data = sonar_data;
+      result = sonar_data;
+   }
+   return(result/118);
 }
